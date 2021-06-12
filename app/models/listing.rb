5b48f70262ccbe910b62efc424c39da27cd568fd
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-06-12 08:54:22 -0700

    adds validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,16 @@
 class Listing < ApplicationRecord
   belongs_to :vessel
+  has_many :rentals
+
+  validates :price, :avail_start, :avail_end, presence: true, eligibility: true
+  validates :description, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if avail_end.blank? || avail_start.blank?
+
+    errors.add(:avail_end, 'must be after the start date') if avail_end < avail_start
+  end
 end
